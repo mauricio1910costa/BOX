d78a97fa1e981f47a2c1aa692a44e79ff460bbe4
Mauricio Costa <mauriciof1910@hotmail.com>
2020-09-12 02:33:56 -0300

    nome enquadrado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BOX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+----------+");
+            Console.WriteLine("| Mauricio |");
+            Console.WriteLine("+----------+");
+        }
+    }
+}
